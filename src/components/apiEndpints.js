6df2b171fda2477437a7bedf8fa1dc1e6f25305a
MuGiMac <mugeshmac@gmailcom>
2025-06-10 16:40:14 +0530

    lastest

--- src/components/apiEndpints.js
+++ src/components/apiEndpints.js
@@ -7,13 +7,13 @@ const AUTH_API = {
 */
 
 const All_SERVERS_API = {
-    ALL_SERVERS: '/AutomationAPI/api/servers/allServers',
+  ALL_SERVERS: '/AutomationAPI/mchp021a/api/servers/allServers',
 };
 
 const CPU_MEMORY_API = {
-	CM_API_021A: 'http://mchp021a.unify.com:9000/AutomationAPI/api/usage/CPUMemoryUsage',
-	CM_API_026A: 'http://mchp026a.unify.com:9000/AutomationAPI/api/usage/CPUMemoryUsage',
-	CM_API_025A: 'http://mchp025a.unify.com:9000/AutomationAPI/api/usage/CPUMemoryUsage',
+	CM_API_021A: '/AutomationAPI/mchp021a/api/usage/CPUMemoryUsage',
+	CM_API_026A: '/AutomationAPI/mchp026a/api/usage/CPUMemoryUsage',
+	CM_API_025A: '/AutomationAPI/mchp025a/api/usage/CPUMemoryUsage',
 };
 
 export {
